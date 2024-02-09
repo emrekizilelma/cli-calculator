8fc338de0de25c5f99c9c09eb837470b6bc802e5
emrekizilelma <emrekizilelma00@gmail.com>
2024-02-09 11:49:38 +0300

    update

--- main.c
+++ main.c
@@ -4,7 +4,23 @@
 
 int main(int argc, char* argv[])
 {
-  calculator_menu();
+  
+
+  int choose = 0;
+
+  while (choose != 5)
+  {
+    calculator_menu();
+
+    printf("-> Make a choose: ");
+    scanf("%d", &choose);
+
+    if(choose == 1)
+    {
+      printf("Result = %.2f\n", add(10, 10));
+    }
+
+  }
 
   return 0;
 }
--- src/calculator_menu.c
+++ src/calculator_menu.c
@@ -2,8 +2,8 @@
 
 void calculator_menu()
 {
-  printf("----------------------------------------\n\n");
-  printf("Welcome the Cli Calculator Program!\n\n");
-  printf("1)Add\n2)Substract\n3)Multiply\n4)Divide\n");
-  printf("----------------------------------------\n\n");
+  printf("-----------------------------------------\n\n");
+  printf("*Welcome the Cli Calculator Program!*\n\n");
+  printf("1)Add\n2)Substract\n3)Multiply\n4)Divide\n5)Exit (q)\n");
+  printf("-----------------------------------------\n\n");
 }
